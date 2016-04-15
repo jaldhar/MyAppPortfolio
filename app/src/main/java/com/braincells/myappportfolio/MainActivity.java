ede804a51308ed7d5da9686fde385c5dd2f77956
Jaldhar H. Vyas <jaldhar@braincells.com>
2016-04-14 23:58:06 -0400

    Remove unused imports.

--- app/src/main/java/com/braincells/myappportfolio/MainActivity.java
+++ app/src/main/java/com/braincells/myappportfolio/MainActivity.java
@@ -1,13 +1,11 @@
 package com.braincells.myappportfolio;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
